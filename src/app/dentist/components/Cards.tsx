681d58583e7fa20249d7d6a101d48c3c7cf7d247
Mateusz Obłoza <mateuszobloza@mac-1.home>
2025-04-06 13:12:48 +0200

    fix images

--- src/app/dentist/components/Cards.tsx
+++ src/app/dentist/components/Cards.tsx
@@ -7,30 +7,35 @@ import Link from "next/link";
 
 export const Cards = () => {
   return (
-    <div className="grid gap-5 desktop-2:gap-12 grid-cols-1 desktop-2:grid-cols-2 mx-auto">
-      <SpecialistCard
-        imageSrc="/images/toothbrush-bg.png"
-        imageAlt="Toothbrush background"
-        icon={<ToothIcon />}
-        shortDescription="Stomatologia dla całej Twojej rodziny pełen zakres usług."
-        title="Stomatolog"
-        name="Justyna Porażko"
-        description="Dla każdego pacjenta przygotowywany jest indywidualny plan leczenia w oparciu o badanie kliniczne oraz badania obrazowe."
-        link="/dentist"
-      />
-
-      <div className="relative max-w-md desktop:max-w-full h-full rounded-[20] overflow-hidden">
-        <Image
-          src="/images/dentist.png"
-          alt="Medical services"
-          className="object-cover"
-          fill
-          quality={100}
+    <div className="grid gap-5 flex justify-center mb-8">
+      <div className="grid gap-5 desktop-2:gap-12 grid-cols-1 desktop-2:grid-cols-2 mx-auto">
+        <SpecialistCard
+          imageSrc="/images/toothbrush-bg.png"
+          imageAlt="Toothbrush background"
+          icon={<ToothIcon />}
+          shortDescription="Stomatologia dla całej Twojej rodziny pełen zakres usług."
+          title="Stomatolog"
+          name="Justyna Porażko"
+          description="Dla każdego pacjenta przygotowywany jest indywidualny plan leczenia w oparciu o badanie kliniczne oraz badania obrazowe."
+          link="/dentist"
         />
-        <div className="absolute bottom-4 right-4">
-          <Link href="/">
-            <Button rightIcon={<BackIcon />}></Button>
-          </Link>
+
+        <div className="group relative max-w-md desktop:max-w-full h-full">
+          <div className="rounded-[20] p-4 tablet:p-5 desktop:p-17 relative overflow-hidden h-full min-h-[400px]">
+            <Image
+              src="/images/dentist.png"
+              alt="Medical services"
+              className="object-cover"
+              fill
+              sizes="(max-width: 768px) 100vw, 50vw"
+              priority
+            />
+            <div className="absolute bottom-4 right-4">
+              <Link href="/">
+                <Button rightIcon={<BackIcon />}></Button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/app/internist/components/Cards.tsx
+++ src/app/internist/components/Cards.tsx
@@ -7,32 +7,36 @@ import { Button } from "@/components/Button";
 
 export const Cards = () => {
   return (
-    <div className="grid gap-5 desktop-2:gap-12 grid-cols-1 desktop-2:grid-cols-2 mx-auto">
-      <div className="relative max-w-md desktop:max-w-full h-full rounded-[20] overflow-hidden">
-        <Image
-          src="/images/dentist.png"
-          alt="Medical services"
-          className="object-cover"
-          fill
-          quality={100}
-        />
-        <div className="absolute bottom-4 right-4">
-          <Link href="/">
-            <Button rightIcon={<BackIcon />}></Button>
-          </Link>
+    <div className="grid gap-5 flex justify-center mb-8">
+      <div className="grid gap-5 desktop-2:gap-12 grid-cols-1 desktop-2:grid-cols-2 mx-auto">
+        <div className="group relative max-w-md desktop:max-w-full h-full">
+          <div className="rounded-[20] p-4 tablet:p-5 desktop:p-17 relative overflow-hidden h-full min-h-[400px]">
+            <Image
+              src="/images/dentist.png"
+              alt="Medical services"
+              className="object-cover"
+              fill
+              quality={100}
+            />
+            <div className="absolute bottom-4 right-4">
+              <Link href="/">
+                <Button rightIcon={<BackIcon />}></Button>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <SpecialistCard
-        imageSrc="/images/medicine-bg.png"
-        imageAlt="Medicine background"
-        icon={<KidneysIcon />}
-        shortDescription="Diagnostyka i leczenie chorób wewnętrznych, z naciskiem na choroby nerek."
-        title="Internista, nefrolog"
-        name="Tomasz Porażko"
-        description="Badanie USG jamy brzusznej obejmujące nerki, drogi moczowe, płuca, naczynia oraz przetoki i grafty do hemodializ."
-        link="/internist"
-      />
+        <SpecialistCard
+          imageSrc="/images/medicine-bg.png"
+          imageAlt="Medicine background"
+          icon={<KidneysIcon />}
+          shortDescription="Diagnostyka i leczenie chorób wewnętrznych, z naciskiem na choroby nerek."
+          title="Internista, nefrolog"
+          name="Tomasz Porażko"
+          description="Badanie USG jamy brzusznej obejmujące nerki, drogi moczowe, płuca, naczynia oraz przetoki i grafty do hemodializ."
+          link="/internist"
+        />
+      </div>
     </div>
   );
 };
