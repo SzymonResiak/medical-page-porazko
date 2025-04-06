edd8d2a9e05c7d75a7cc4c408a9f419b39944b60
Mateusz Obłoza <mateuszobloza@mac-1.home>
2025-04-06 10:34:56 +0200

    location

--- src/app/dentist/components/Location.tsx
+++ src/app/dentist/components/Location.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const Location = () => {
   return (
     <div>
@@ -7,6 +9,22 @@ export const Location = () => {
           na <br /> mapie
         </span>
       </p>
+      <div className="bg-[#2E2E2E] rounded-[20] p-[30px] mt-5">
+        <p className="text-[#FCFCFC] text-sm">
+          <span className="font-bold">Dom Medyczny &quot;Pro Corde&quot;</span>,
+          gabinet znajdue się na
+          <br /> pierwszym piętrze
+        </p>
+      </div>
+      <div className="mt-5">
+        <Image src="/images/map.png" alt="Map" width={500} height={500} />
+      </div>
+      <div className="mt-5">
+        <p className="text-xl text-right">
+          ul. Piłsudskiego 11A,
+          <br /> 45-706 Opole
+        </p>
+      </div>
     </div>
   );
 };
